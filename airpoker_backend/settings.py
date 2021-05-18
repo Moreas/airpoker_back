864904b13c15efb2092732250902d9c651b0ca48
Moreas <florent.pignaud@pfj-services.com>
2021-05-17 21:34:50 -0400

    init recreated

--- airpoker_backend/settings.py
+++ airpoker_backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '599w%#bl4fwh8mg*#cb9kme#&6vpmjg6k!$cc^$5wvgij368f-'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
